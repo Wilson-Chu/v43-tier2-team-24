48cae87868cfdf3bc718dac3a592727d661e6c1f
wilson <wilson1.chu@gmail.com>
2023-04-18 18:00:39 -0400

    deployed app

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 // added 'server: port 3000' instead of default port 5173 for futureproofing.
 export default defineConfig({
-  plugins: [react()],
   base: "/v43-tier2-team-24/",
+  plugins: [react()],
   server: {
     port: 3000,
   },
